249c784caddf0e1533f44b3d7658d59c855c1777
youngbeom94 <darania@kookmin.ac.kr>
2021-10-18 00:42:46 +0900

    Add Entropy Get and Test

--- genLibrary/ctr_drbg.c
+++ genLibrary/ctr_drbg.c
@@ -1,6 +1,12 @@
 #include "YBCrypto.h"
 #include "ctr_drbg.h"
 
+extern int32_t YBCRYPTO_STATE;
+extern IS_ALG_TESTED algTestedFlag;
+extern int32_t Inner_API_GetState(void);
+extern void YBCrypto_ChangeState(int32_t newState);
+CipherManager CM;
+
 DRBGManager DM;
 
 //EOF
